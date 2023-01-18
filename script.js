7c90e246f7631f6e1a3eac192233ea552f43fa12
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-01-18 13:05:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+
+function major(arr)
+{
+  //your code here
+   let reqF = Math.floor(arr.length/ 2);
+        let freq = {};
+
+        for(let i = 0; i< arr.length; i++){
+                if(!freq[arr[i]] ){
+                        freq[arr[i]] = 1;
+                }else{
+                        freq[arr[i]] = freq[arr[i]] +1;
+                }
+                if(freq[arr[i]] > reqF){
+                        return arr[i];
+                }
+                
+        }
+        return -1;
+}
+module.exports=major;
